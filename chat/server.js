d8f1690fff129b050c562efe4fd451de097afc1a
v4rts <v4rts@yandex.ru>
2021-02-11 13:31:34 +0300

    user connect with socket

--- chat/server.js
+++ chat/server.js
@@ -1,6 +1,19 @@
-const express =  require("express");
-
+const express =  require('express');
 const app = express();
+const server = require('http').Server(app); /* Set expressapp for server */
+const io = require('socket.io')(server,{
+
+    cors: {
+
+        origin: '*',
+        methods: ["GET", "POST"],
+        credentials: true,
+        transports: ['websocket']
+
+    }
+
+});
+/*const webSocket = io(server);*/
 
 const rooms = new Map();
 
@@ -8,7 +21,11 @@ app.get('/rooms', (req, res) => {
     res.json(rooms);
 });
 
-app.listen(8888, (err) => {
+io.on('connection', socket => { /* Connection testing */
+    console.log("user connected", socket)
+})
+
+server.listen(8888, (err) => {
     if (err) {
         throw Error(err);
     }
