0c920cbdb5c92b789a5830d3c45ae1d595730260
v4rts <v4rts@yandex.ru>
2021-02-16 12:43:02 +0300

    states for roomID & userName

--- chat/src/comp/Start.jsx
+++ chat/src/comp/Start.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import socket from "../socket";
 
 let Start = () => {
+
+    const [roomID, setRoomID] = useState('');
+    const [userName, setUserName] = useState('');
+
+    const onGo = () => {
+        if (!roomID || !userName){
+            alert("Required fields are missing")
+        }
+        console.log(roomID);
+        console.log(userName);
+    }
+
     return (
     <div className="wrapper">
-        <input type="text" placeholder="Room" value=""/>
-        <input type="text" placeholder="User" value=""/>
-        <button>Go</button>
+        <input type="text" placeholder="Room" value={roomID} onChange={(e) => setRoomID(e.target.value)}/>
+        <input type="text" placeholder="User" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+        <button onClick={onGo}>Go</button>
     </div>
     );
 }
